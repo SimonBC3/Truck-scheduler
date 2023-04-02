a8bfc9727d17e786dae72b57d0998d56c678e2a5
SkyRexDEV <skyrexhd@gmail.com>
2023-04-02 19:36:26 +0200

    Added args input

--- truckscheduler.py
+++ truckscheduler.py
@@ -1,14 +1,21 @@
 from math import sqrt, pow, inf
 
 import pyhop
+import argparse
+
+TRUCK_VALID_POSITIONS = ["Huelva", "Cadiz", "Sevilla",
+                         "Cordoba", "Malaga", "Jaen", "Granada", "Almeria"]
+ALL_CITIES = ["Huelva", "Cadiz", "Sevilla",
+              "Cordoba", "Malaga", "Jaen", "Granada", "Almeria", "JerezDeLaFrontera", "Osuna", "Guadix", "Alcaudete"]
 
 
 def choose_by_connection(state):
     for driver in state.drivers:
         for truck in state.trucks:
-           if state.trucks[truck]['location'] in state.sideways[state.drivers[driver]['location']]:
+            if state.trucks[truck]['location'] in state.sideways[state.drivers[driver]['location']]:
                 return {'driver': driver, 'truck': truck}
     return {}
+
 
 def choose_by_distance(state):
     bestDistance = 0.0
@@ -18,22 +25,25 @@ def choose_by_distance(state):
         for truck in state.trucks:
             driverLocation = state.drivers[driver]['location']
             truckLocation = state.trucks[truck]['location']
-            currentDistance = distance(state.coordinates[driverLocation], state.coordinates[truckLocation] )
+            currentDistance = distance(
+                state.coordinates[driverLocation], state.coordinates[truckLocation])
             if bestDistance == 0:
                 bestDistance = currentDistance
                 bestDriver = driver
                 bestTruck = truck
-            else: 
+            else:
                 if currentDistance < bestDistance:
                     bestDistance = currentDistance
                     bestDriver = driver
                     bestTruck = truck
     return {'driver': bestDriver, 'truck': bestTruck}
 
+
 def distance(c1, c2):
     x = pow(c1['X'] - c2['X'], 2)
     y = pow(c1['Y'] - c2['Y'], 2)
     return sqrt(x + y)
+
 
 def select_new_city(state, currentLocation, goal):  # evaluation function
     best = inf  # big float
@@ -60,6 +70,7 @@ def select_new_city_sideway(state, currentLocation, goal):
                 best = currentCost
     return best_city
 
+
 def walk_op(state, driver, selectedCity):
     currentDrivertLocation = state.drivers[driver]['location']
     if state.walkpath == []:
@@ -70,6 +81,7 @@ def walk_op(state, driver, selectedCity):
         return state
     return False
 
+
 def get_on_bus_op(state, driver):
     for bus in state.buses:
         if state.buses[bus]['location'] == state.drivers[driver]['location']:
@@ -77,6 +89,7 @@ def get_on_bus_op(state, driver):
             state.drivers[driver]['location'] = 'on_bus'
             return bus
     return False
+
 
 def travel_by_bus_op(state, bus, selectedCity):
     currentBusLocation = state.buses[bus]['location']
@@ -86,11 +99,13 @@ def travel_by_bus_op(state, bus, selectedCity):
         return state
     return False
 
+
 def get_off_bus_op(state, truck, driver, bus):
     currentBusLocation = state.buses[bus]['location']
     if state.drivers[driver]['location'] == 'on_bus' and currentBusLocation == state.trucks[truck]['location']:
         state.drivers[driver]['location'] = currentBusLocation
         return state
+
 
 def travel_op(state, truck, driver, selectedCity):
     currentTruckLocation = state.trucks[truck]['location']
@@ -104,6 +119,7 @@ def travel_op(state, truck, driver, selectedCity):
         return state
     else:
         return False
+
 
 def get_on_truck_op(state, truck, driver):
     if state.drivers[driver]['location'] == state.trucks[truck]['location']:
@@ -133,11 +149,6 @@ def drop_package_op(state, goal, truck, package):
         return state
 
 
-pyhop.declare_operators(travel_op, walk_op, get_on_truck_op,
-                        get_off_truck_op, gather_package_op, drop_package_op, get_on_bus_op, travel_by_bus_op, get_off_bus_op)
-print()
-pyhop.print_operators()
-
 def walk_to_truck(state, goal, truck, driver):
     driverCurrentLocation = state.drivers[driver]['location']
     truckCurrentLocation = state.trucks[truck]['location']
@@ -147,29 +158,34 @@ def walk_to_truck(state, goal, truck, driver):
         return [('walk_op', driver, selectedCity), ('travel_to_truck_on_foot', goal, truck, driver)]
     return [('start_delivery', goal, truck, driver)]
 
+
 def already_on_truck(state, goal, truck, driver):
     if state.drivers[driver]['location'] == state.trucks[truck]['location'] and state.drivers[driver]['location']:
         return []
     return False
+
 
 def travel_by_bus(state, goal, truck, driver, bus):
     if bus == '':
         bus = get_on_bus_op(state, driver)
         print('---selectedbus----' + bus)
         return [('travel_to_truck_by_bus', goal, truck, driver, bus)]
-    
+
     busLocation = state.buses[bus]['location']
     truckLocation = state.trucks[truck]['location']
 
     if busLocation != truckLocation:
-        selectedCity = select_new_city_sideway(state, busLocation, truckLocation)
-        return [('travel_by_bus_op', bus, selectedCity ), ('travel_to_truck_by_bus', goal, truck, driver, bus)]
-    else: 
+        selectedCity = select_new_city_sideway(
+            state, busLocation, truckLocation)
+        return [('travel_by_bus_op', bus, selectedCity), ('travel_to_truck_by_bus', goal, truck, driver, bus)]
+    else:
         return [('get_off_bus_op', truck, driver, bus), ('start_delivery', goal, truck, driver)]
+
 
 def travel_by_truck(state, goal, truck, driver):
     return [('get_on_truck_op', truck, driver), ('all_gathered', goal, truck, driver),
             ('finish_delivery', goal, truck, driver), ('travel_to_city', goal, truck, driver), ('get_off_truck_op', driver, truck, goal)]
+
 
 def travel_m(state, goal, truck, driver):
     truckCurrentLocation = state.trucks[truck]['location']
@@ -179,6 +195,7 @@ def travel_m(state, goal, truck, driver):
             state, truckCurrentLocation, truckGoalLocation)
         return [('travel_op', truck, driver, selectedCity), ('travel_to_city', goal, truck, driver)]
     return False
+
 
 def travel_to_package_m(state, goal, truck, driver, package):
     truckCurrentLocation = state.trucks[truck]['location']
@@ -223,78 +240,122 @@ def deliver_package_m(state, goal, truck, driver, package):
         return [('drop_package_op', goal, truck, package)]
 
 
-pyhop.declare_methods('travel_to_truck_on_foot', walk_to_truck, already_on_truck)
-pyhop.declare_methods('travel_to_truck_by_bus', travel_by_bus)
-pyhop.declare_methods('start_delivery', travel_by_truck)
-pyhop.declare_methods('all_gathered', all_gathered)
-pyhop.declare_methods('travel_to_package', travel_to_package_m)
-pyhop.declare_methods('finish_delivery', all_delivered)
-pyhop.declare_methods('deliver_package', deliver_package_m)
-pyhop.declare_methods('travel_to_city', travel_m, already_there)
-
-
 def choose_variables(state, goal):
     answer = choose_by_connection(state)
     if answer != {}:
-        #walkToTruck
+        # walkToTruck
         return [('travel_to_truck_on_foot', goal, answer['truck'], answer['driver'])]
-    
+
     answer = choose_by_distance(state)
     if answer != {}:
-        #takeBusToTruck
+        # takeBusToTruck
         return [('travel_to_truck_by_bus', goal, answer['truck'], answer['driver'], '')]
     return False
 
 
-pyhop.declare_methods('choose_variables', choose_variables)
-print()
-pyhop.print_methods()
+def main(args):
 
-# INITIAL STATE
+    # Operators declaration
+    pyhop.declare_operators(travel_op, walk_op, get_on_truck_op,
+                            get_off_truck_op, gather_package_op, drop_package_op, get_on_bus_op, travel_by_bus_op, get_off_bus_op)
+    # Method declaration
+    pyhop.declare_methods('travel_to_truck_on_foot',
+                          walk_to_truck, already_on_truck)
+    pyhop.declare_methods('travel_to_truck_by_bus', travel_by_bus)
+    pyhop.declare_methods('start_delivery', travel_by_truck)
+    pyhop.declare_methods('all_gathered', all_gathered)
+    pyhop.declare_methods('travel_to_package', travel_to_package_m)
+    pyhop.declare_methods('finish_delivery', all_delivered)
+    pyhop.declare_methods('deliver_package', deliver_package_m)
+    pyhop.declare_methods('travel_to_city', travel_m, already_there)
+    pyhop.declare_methods('choose_variables', choose_variables)
 
-state1 = pyhop.State('state1')
-state1.coordinates = {'Huelva': {'X': 25, 'Y': 275}, 'Cadiz': {'X': 200, 'Y': 50}, 'Sevilla': {'X': 250, 'Y': 325},
-                      'Cordoba': {'X': 475, 'Y': 450}, 'Malaga': {'X': 550, 'Y': 100}, 'Jaen': {'X': 750, 'Y': 425},
-                      'Granada': {'X': 800, 'Y': 250}, 'Almeria': {'X': 1000, 'Y': 150}, 'JerezDeLaFrontera': {'X': 225, 'Y': 300},
-                      'Osuna': {'X': 300, 'Y': 300}, 'Alcaudete': {'X': 675, 'Y': 400}, 'Guadix': {'X': 900, 'Y': 350}}
+    # Initial State
+    state1 = pyhop.State('state1')
+    state1.coordinates = {'Huelva': {'X': 25, 'Y': 275}, 'Cadiz': {'X': 200, 'Y': 50}, 'Sevilla': {'X': 250, 'Y': 325},
+                          'Cordoba': {'X': 475, 'Y': 450}, 'Malaga': {'X': 550, 'Y': 100}, 'Jaen': {'X': 750, 'Y': 425},
+                          'Granada': {'X': 800, 'Y': 250}, 'Almeria': {'X': 1000, 'Y': 150}, 'JerezDeLaFrontera': {'X': 225, 'Y': 300},
+                          'Osuna': {'X': 300, 'Y': 300}, 'Alcaudete': {'X': 675, 'Y': 400}, 'Guadix': {'X': 900, 'Y': 350}}
 
-state1.roads = {'Huelva': {'Sevilla'}, 'Sevilla': {'Cadiz', 'Huelva', 'Cordoba', 'Malaga'},
-                'Cadiz': {'Sevilla', 'Malaga'}, 'Cordoba': {'Sevilla', 'Malaga', 'Jaen'},
-                'Malaga': {'Cadiz', 'Huelva', 'Cordoba', 'Sevilla', 'Granada', 'Almeria'},
-                'Jaen': {'Cordoba', 'Granada'}, 'Granada': {'Jaen', 'Malaga', 'Almeria'},
-                'Almeria': {'Granada', 'Malaga'}}
+    state1.roads = {'Huelva': {'Sevilla'}, 'Sevilla': {'Cadiz', 'Huelva', 'Cordoba', 'Malaga'},
+                    'Cadiz': {'Sevilla', 'Malaga'}, 'Cordoba': {'Sevilla', 'Malaga', 'Jaen'},
+                    'Malaga': {'Cadiz', 'Huelva', 'Cordoba', 'Sevilla', 'Granada', 'Almeria'},
+                    'Jaen': {'Cordoba', 'Granada'}, 'Granada': {'Jaen', 'Malaga', 'Almeria'},
+                    'Almeria': {'Granada', 'Malaga'}}
 
-state1.sideways = {'Huelva': {'JerezDeLaFrontera'}, 'Sevilla': {'JerezDeLaFrontera', 'Osuna'},
-                   'Cadiz': {'JerezDeLaFrontera', 'Osuna'}, 'Cordoba': {'Alcaudete'},
-                   'Malaga': {'Osuna', 'Alcaudete'},
-                   'Jaen': {'Alcaudete', 'Guadix'}, 'Granada': {'Gaudix', 'Alcaudete'},
-                   'Almeria': {'Guadix'}, 'JerezDeLaFrontera': {'Cadiz', 'Osuna', 'Huelva', 'Sevilla'},
-                   'Osuna': {'JerezDeLaFrontera', 'Cadiz', 'Sevilla', 'Alcaudete', 'Malaga'},
-                   'Alcaudete': {'Malaga', 'Osuna', 'Cordoba', 'Jaen', 'Granada'},
-                   'Guadix': {'Granada', 'Jaen', 'Almeria'}}
+    state1.sideways = {'Huelva': {'JerezDeLaFrontera'}, 'Sevilla': {'JerezDeLaFrontera', 'Osuna'},
+                       'Cadiz': {'JerezDeLaFrontera', 'Osuna'}, 'Cordoba': {'Alcaudete'},
+                       'Malaga': {'Osuna', 'Alcaudete'},
+                       'Jaen': {'Alcaudete', 'Guadix'}, 'Granada': {'Gaudix', 'Alcaudete'},
+                       'Almeria': {'Guadix'}, 'JerezDeLaFrontera': {'Cadiz', 'Osuna', 'Huelva', 'Sevilla'},
+                       'Osuna': {'JerezDeLaFrontera', 'Cadiz', 'Sevilla', 'Alcaudete', 'Malaga'},
+                       'Alcaudete': {'Malaga', 'Osuna', 'Cordoba', 'Jaen', 'Granada'},
+                       'Guadix': {'Granada', 'Jaen', 'Almeria'}}
 
-state1.buses = {'b0': {'location': 'Huelva', 'price': 3}, 'b1': {'location': 'Cadiz', 'price': 3},
-                'b2': {'location': 'Sevilla', 'price': 3}, 'b3': {'location': 'Cordoba', 'price': 3},
-                'b4': {'location': 'Malaga', 'price': 3}, 'b5': {'location': 'Jaen', 'price': 3},
-                'b5': {'location': 'Granada', 'price': 3}, 'b6': {'location': 'Almeria', 'price': 3}}
+    state1.buses = {'b0': {'location': 'Huelva', 'price': args.price}, 'b1': {'location': 'Cadiz', 'price': args.price},
+                    'b2': {'location': 'Sevilla', 'price': args.price}, 'b3': {'location': 'Cordoba', 'price': args.price},
+                    'b4': {'location': 'Malaga', 'price': args.price}, 'b5': {'location': 'Jaen', 'price': args.price},
+                    'b5': {'location': 'Granada', 'price': args.price}, 'b6': {'location': 'Almeria', 'price': args.price}}
 
-state1.packages = {'p1': {'location': 'Sevilla'}, 'p2': {'location': 'Malaga'}, 'p3': {'location': 'Jaen'}}
+    state1.packages = {f'p{i+1}': {'location': loc}
+                       for i, loc in enumerate(args.ppos)}
 
-state1.drivers = {'d1': {'location': 'Sevilla'}, 'd2': {'location': 'Guadix'}}
-state1.trucks = {'t0': {'location': 'Almeria'}, 't1': {'location': 'Cordoba'}}
+    state1.drivers = {f'd{i+1}': {'location': loc}
+                      for i, loc in enumerate(args.dpos)}
 
-state1.path = []
-state1.walkpath = []
-state1.buspath = []
-state1.cost = 0
+    state1.trucks = {f't{i+1}': {'location': loc}
+                     for i, loc in enumerate(args.tpos)}
 
-# GOAL
-goal1 = pyhop.Goal('goal1')
-goal1.packages = {'p1': {'location': 'Jaen'}, 'p2': {'location': 'Cordoba'}, 'p3': {'location': 'Malaga'}}
-goal1.drivers = {'d1': {'location': 'Sevilla'}, 'd2': {'location': 'Sevilla'}}
-goal1.trucks = {'t0': {'location': 'Sevilla'}, 't1': {'location': 'Sevilla'}}
+    state1.path = []
+    state1.walkpath = []
+    state1.buspath = []
+    state1.cost = 0
+
+    # Goal
+    goal1 = pyhop.Goal('goal1')
+    goal1.packages = {f'p{i+1}': {'location': loc}
+                      for i, loc in enumerate(args.delivery)}
+
+    goal1.drivers = {f'd{i+1}': {'location': loc}
+                     for i, loc in enumerate(args.destination)}
+    goal1.trucks = {f't{i+1}': {'location': loc}
+                    for i, loc in enumerate(args.destination)}
+
+    pyhop.pyhop(state1, [('choose_variables', goal1)], verbose=1)
 
 
-# print('- If verbose=3, Pyhop also prints the intermediate states:')
+def check_args(args):
+    if (len(args.destination) == len(args.dpos) == len(args.tpos) and len(args.delivery) == len(args.ppos)):
+        return True
+    else:
+        raise Exception(f"Check that the number of specified arguments match.")
 
-result = pyhop.pyhop(state1, [('choose_variables', goal1)], verbose=1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dpos", required=True,
+                        help="Drivers' initial positions",
+                        nargs="+",
+                        choices=ALL_CITIES)
+    parser.add_argument("--tpos", required=True,
+                        help="Trucks' initial positions",
+                        nargs="+",
+                        choices=TRUCK_VALID_POSITIONS)
+    parser.add_argument("--destination", required=True,
+                        help="Drivers' and trucks' final destinations. Trucks and drivers share the same destination.",
+                        nargs="+",
+                        choices=TRUCK_VALID_POSITIONS)
+    parser.add_argument("--ppos", required=True,
+                        help="Packages' initial positions",
+                        nargs="+",
+                        choices=TRUCK_VALID_POSITIONS)
+    parser.add_argument("--delivery", required=True,
+                        help="Packages' final destination",
+                        nargs="+",
+                        choices=TRUCK_VALID_POSITIONS)
+    parser.add_argument("--price", required=True,
+                        help="Bus tickets' price in €. It's the same in all cities.")
+
+    args = parser.parse_args()
+    if check_args(args):
+        main(args)
